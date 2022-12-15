2c830fb8c87af9c26d09206c443537d4468a5b65
buinam2001 <buinam511201@gmail.com>
2022-12-15 08:48:32 +0700

    update flie

--- js/index.js
+++ js/index.js
@@ -9,9 +9,6 @@ function handlemenu() {
     menu.style.left = 0;
     coating.style.display = "block";
 
-
-    
-    
 }
 
 
@@ -21,8 +18,7 @@ function handleclose() {
     menu.style.left = '-100%';
     menu.style.top = 0;
     menu.style.bottom = 0;
-
-
     coating.style.display = "none";
+   
     
 }
